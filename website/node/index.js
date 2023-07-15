9f3d211dbe1e9780004ba75c28d53966abb45fa7
Philip Samoei <samphilkerich04@gmail.com>
2023-07-15 11:14:49 +0300

    adding json data

--- website/node/index.js
+++ website/node/index.js
@@ -1,6 +1,9 @@
-const math = require('./math')
-const {add, subtract} = math
+// const math = require('./math')
+// const {add, subtract} = math
  
-console.log( add(2,3))
-console.log( subtract(2,3))
+// console.log( add(2,3))
+// console.log( subtract(2,3))
 
+const data  = require("./data.json")
+
+console.log(data)
